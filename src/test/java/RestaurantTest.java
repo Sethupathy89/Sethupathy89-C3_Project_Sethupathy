16838397c1683f2dcece8919169b80c03e304f26
Sethupathy89 <sethupathysr@gmail.com>
2021-06-01 17:45:56 +0530

    Part 2:Solution

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -19,11 +19,13 @@ class RestaurantTest {
 	LocalTime openingTime;
 	LocalTime closingTime;
 	int initialMenuSize;
+	LocalTime originalTime;
 	
 	@BeforeEach
 	public void setUp() {
 		openingTime = LocalTime.parse("10:30:00");
 		closingTime = LocalTime.parse("22:00:00");
+		originalTime = LocalTime.now();
 		restaurant  = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
 		restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
@@ -33,7 +35,6 @@ class RestaurantTest {
 
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time() throws Exception{ 
-    	LocalTime originalTime = LocalTime.now();
     	long timeCalc = ChronoUnit.MINUTES.between(originalTime,openingTime)+50;
     	LocalTime mockTime = originalTime.plusMinutes(timeCalc);
     	Mockito.when(mockRestaurant.getCurrentTime()).thenReturn(mockTime);
@@ -42,7 +43,6 @@ class RestaurantTest {
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time() throws Exception{
-    	LocalTime originalTime = LocalTime.now();
     	long timeCalc = ChronoUnit.MINUTES.between(openingTime,originalTime)+50;
     	LocalTime mockTime = originalTime.minusMinutes(timeCalc);
     	Mockito.when(mockRestaurant.getCurrentTime()).thenReturn(mockTime);
